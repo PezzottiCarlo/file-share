e8ad73ba80e015cea5492bbe41a90b48936c7a10
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-05-08 19:42:28 +0200

    feat: :sparkles: server get file list

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const config = require('./config/config');
+const file = new (require('./util/file'))(config.file_path);
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(config.port, () => {
+    console.log('Server is running on port ' + config.port);
+})
+
+app.get('/list', (req, res) => {
+    res.send(file.list());
+})
--- server/util/file.js
+++ server/util/file.js
@@ -5,9 +5,6 @@ module.exports = class File {
         if(!this.#path_exist(file_path)){
             this.#create_path(file_path);
         }
-        if(!this.#path_access(file_path)){
-            throw new Error('File path is not accessible');
-        }
         if(this.#path_is_file(file_path)){
             throw new Error('File path is a file');
         }
@@ -22,13 +19,6 @@ module.exports = class File {
 
     #file_exist(file_name){
         if(!fs.existsSync(this.file_path+'/'+file_name)){
-            return false;
-        }
-        return true;
-    }
-
-    #path_access(){
-        if(!fs.accessSync(this.file_path, fs.constants.R_OK)){
             return false;
         }
         return true;
@@ -51,9 +41,6 @@ module.exports = class File {
 
     #primary_check(){
         if(!this.#path_exist(this.file_path)){
-            return false;
-        }
-        if(!this.#path_access(this.file_path)){
             return false;
         }
         if(this.#path_is_file(this.file_path)){
