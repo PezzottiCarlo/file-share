96332a13581763aef1fbadeef84c5fda39326da6
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-05-08 19:18:25 +0200

    feat: :sparkles: file creation

--- server/util/file.js
+++ server/util/file.js
@@ -13,32 +13,47 @@ module.exports = class File {
         }
     }
 
-    #path_exist(file_path){
-        if(!fs.existsSync(file_path)){
+    #path_exist(){
+        if(!fs.existsSync(this.file_path)){
             return false;
         }
         return true;
     }
 
-    #path_access(file_path){
-        if(!fs.accessSync(file_path, fs.constants.R_OK)){
+    #path_access(){
+        if(!fs.accessSync(this.file_path, fs.constants.R_OK)){
             return false;
         }
         return true;
     }
 
-    #path_is_file(file_path){
-        if(!fs.lstatSync(file_path).isFile()){
+    #path_is_file(){
+        if(!fs.lstatSync(this.file_path).isFile()){
             return false;
         }
         return true;
     }
 
-    #create_path(file_path){
-        if(!this.#path_exist(file_path)){
-            fs.mkdirSync(file_path);
+    #create_path(){
+        if(!this.#path_exist(this.file_path)){
+            fs.mkdirSync(this.file_path);
             return true;
         }
         return false;
+    }
+
+
+    create_file(file_name){
+        if(!this.#path_exist(this.file_path)){
+            return false;
+        }
+        if(!this.#path_access(this.file_path)){
+            return false;
+        }
+        if(this.#path_is_file(this.file_path)){
+            return false;
+        }
+        fs.writeFileSync(this.file_path + '/' + file_name, '');
+        return true;
     }
 }
